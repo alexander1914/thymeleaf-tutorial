0af7de10ea6a59c805bd6041828a383cd350ca62
alexander1914 <alexander.oliveira99@gmail.com>
2026-07-16 22:15:16 +0100

    [Thymeleaf introduction] - I had been learning more contents about thymeleaf today

---
+++ src/main/java/com/github/alexander1914/thymeleaf_tutorial/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.github.alexander1914.thymeleaf_tutorial.controller;
+
+import com.github.alexander1914.thymeleaf_tutorial.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    /// Handler method to handle variable-expression request
+    // http://localhost:8080/variable-expression
+    @GetMapping("variable-expression")
+    public String variableExpression(Model model) {
+        User user = new User("Alexander", "alexander.oliveira99@gmail.com", "ADMIN", "Male");
+        model.addAttribute("user", user);
+        return "variable-expression";
+    }
+
+    /// Handler method to handle selection expression
+    // http://localhost:8080/selection-expression
+    @GetMapping("selection-expression")
+    public String selectionExpression(Model model){
+        User user = new User("Alexander", "alexander.oliveira99@gmail.com", "ADMIN", "Male");
+        model.addAttribute("user", user);
+        return "selection-expression";
+    }
+}
